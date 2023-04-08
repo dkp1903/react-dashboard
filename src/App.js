232b3361c33a2380a3d39f4a339e51d28ba109ff
dkp1903 <dushyantpathak6789@gmail.com>
2023-04-08 21:13:14 +0000

    bg to settings

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ const App = () => {
     <div>
       <BrowserRouter>
       <div className='flex relative dark:bg-main-dark-bg'>
-    <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
-    <TooltipComponent content="Settings" position="Top">
-      <button>
-        <FiSettings />
-      </button>
-    </TooltipComponent>
-    </div>
+        <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+          <TooltipComponent content="Settings" position="Top">
+            <button type='button'
+                    className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                    style={{ background: 'blue', borderRadius: '50%'}}>
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
       </div>
       </BrowserRouter>
       </div>
